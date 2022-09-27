c96eab5a4cfda65957f7b81f592e2e35830cab83
Rashadfrd <rashadnf@code.edu.az>
2022-09-28 02:22:59 +0400

    admin area added

---
+++ NestProject/Areas/Manage/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NestProject.Areas.Manage.ViewModel;
+using NestProject.DAL;
+
+namespace NestProject.Areas.Manage.Controllers
+{
+    [Area("Manage")]
+    public class ProductController : Controller
+    {
+        private NestContext _context { get; }
+        public ProductController(NestContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            AdminProductVM vm = new AdminProductVM();
+            vm.Categories = _context.Categories.ToList();
+            vm.Products = _context.Products.Include(x=>x.ProductImages).ToList();            
+            return View(vm);
+        }
+    }
+}
---
+++ NestProject/Areas/Manage/ViewModel/AdminProductVM.cs
@@ -0,0 +1,10 @@
+﻿using NestProject.Models;
+
+namespace NestProject.Areas.Manage.ViewModel
+{
+    public class AdminProductVM
+    {
+        public List<Product> Products { get; set; }
+        public List<Category> Categories { get; set; }
+    }
+}
