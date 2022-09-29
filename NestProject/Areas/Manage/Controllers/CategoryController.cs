52a25d1160201322a4a0ab19e5d189b6252599cd
Rashadfrd <rashadnf@code.edu.az>
2022-09-29 15:54:09 +0400

    category and sliders partially crud added

---
+++ NestProject/Areas/Manage/Controllers/CategoryController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NestProject.DAL;
+using NestProject.Models;
+
+namespace NestProject.Areas.Manage.Controllers
+{
+    [Area("Manage")]
+    public class CategoryController : Controller
+    {
+        private NestContext _context;
+        public CategoryController(NestContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var categories = _context.Categories.Include(x=>x.Products);
+            return View(categories);
+        }
+
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddCategory(Category category)
+        {
+            category.ImageUrl = " ";            
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        public IActionResult Delete(int id)
+        {
+            var category = _context.Categories.Find(id);
+            if (category.IsDeleted == false)
+            {
+                category.IsDeleted = true;
+            }
+            else
+            {
+            _context.Categories.Remove(category);
+            }
+            _context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ NestProject/Areas/Manage/Controllers/SliderController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NestProject.DAL;
+
+namespace NestProject.Areas.Manage.Controllers
+{
+    [Area("Manage")]
+    public class SliderController : Controller
+    {
+        private NestContext _context { get; }
+        public SliderController(NestContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var sliders = _context.Sliders.OrderBy(x=>x.Order);
+            return View(sliders);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
